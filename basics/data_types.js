59672895fd3d5ea0c4b28e4182191a4c473c06f6
vinayydv3695 <Vinayydv343@gmail.com>
2024-06-04 09:00:23 +0530

    Javascript data types practice

---
+++ basics/data_types.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+//alert(3+3)  we are currently using nodejs instead an browser
+console.log(3+3);
+
+console.log("Baka");
+
+
+
+
+let name ="Zurako";  // string data type
+let age = 19        //int data type  ranges from 2 to  the power 53
+let IsLoggedin= false;   // boolean data type true/false
+
+//null = stand alone value
+//undefined= in the previous program we have letaccountState; so this is a null value
+// symbol= uniqueness
+//objects= 
+
+console.log(typeof "Baka");  // string
+console.log(typeof null);
+console.log(typeof undefined);
