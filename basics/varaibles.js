677663cd6e121c88b5af7ee7f718ce176b852e23
vinayydv3695 <Vinayydv343@gmail.com>
2024-06-04 08:30:54 +0530

    Javascript varaibles and constants

--- basics/varaibles.js
+++ basics/varaibles.js
@@ -1,9 +1,12 @@
 const accountID = 11243;
 let accountEmail = "kamisama@gmail.com";
 var accountpassword = "L8QLYCQCP";
-var accountcCity = "EDO"; 
+ accountcCity = "EDO"; 
+ let accountState;
 
 // accountID = 23 not allowed (comment explaining why this is not allowed)
-
+// never use var for declaring varaibles always use let be ause var used in acnient times .
 console.log(accountID); 
-console.table([accountID, accountpassword, accountcCity]); 
+console.table([accountID, accountpassword, accountcCity,accountState]); 
+console.log(accountEmail);
+
