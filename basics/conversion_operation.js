c8e5549be75de580cfa825cf790eea4a485b1e6f
vinayydv3695 <Vinayydv343@gmail.com>
2024-06-04 09:23:44 +0530

    Javascript conversion operations

---
+++ basics/conversion_operation.js
@@ -0,0 +1,26 @@
+let score= "33sfdvbf";
+console.log(typeof (score))
+
+let valueINumber = Number(score);
+
+console.log(typeof valueINumber);
+console.log(valueINumber);
+
+
+// if the score is null then it will print 0
+// and in case of there is an string it has some values like 334y then it will convert int Nan 
+// same goes for undefined it will show NaN
+// integer like 33 will give value 33
+let IsLoggedin= "baka";
+let booleanIsLoggedin= Boolean(IsLoggedin)
+
+console.log(booleanIsLoggedin);
+// 0>= false
+//1>= true
+// ""= false
+// "some value">= true
+let maxer = 33;
+let zura = String(maxer);
+console.log(maxer);
+console.log(typeof(maxer));
+
