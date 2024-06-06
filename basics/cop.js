58a7fb4ff44ea89451e3998b134748d9fd14e5d6
vinayydv3695 <Vinayydv343@gmail.com>
2024-06-06 12:52:34 +0530

    Javascript  practice for operations

---
+++ basics/cop.js
@@ -0,0 +1,21 @@
+// Conversion operations in JavaScript
+
+// Convert string to number
+let stringNumber = "123";
+let number = Number(stringNumber);
+console.log("Converted string to number:", number);
+
+// Convert number to string
+let num = 456;
+let str = String(num);
+console.log("Converted number to string:", str);
+
+// Convert string to boolean
+let strBoolean = "true";
+let booleanValue = Boolean(strBoolean);
+console.log("Converted string to boolean:", booleanValue);
+
+// Convert boolean to string
+let bool = true;
+let boolStr = String(bool);
+console.log("Converted boolean to string:", boolStr);
