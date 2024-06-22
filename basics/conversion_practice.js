7a2ba421abb4098ff47a031d4bf28cb334625e0c
vinayydv3695 <vinayydv343@gmail.com>
2024-06-22 18:50:15 +0530

    Javascript conversion operations

--- basics/conversion_practice.js
+++ basics/conversion_practice.js
@@ -2,5 +2,5 @@
     console.log(typeof(salary));
     
        let promtion= Number(salary);
-           console.log(typeof(promtion)
+           console.log(typeof(promtion))
           console.log(promtion)
---
+++ basics/operations.js
@@ -0,0 +1,11 @@
+let value = 3
+let negvalue = -value
+//console.log(neglvalue);
+
+console.log(2+2);
+console.log(2*2);
+console.log(2**2);
+console.log(2-2);
+console.log(2+-2);
+console.log(2/2);
+console.log(2%2);
