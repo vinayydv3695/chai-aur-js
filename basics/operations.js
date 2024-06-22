37d8f083fa2469a62164ab299dccca169ff118e0
vinayydv3695 <vinayydv343@gmail.com>
2024-06-22 18:53:45 +0530

    Javascript conversion operations

--- basics/operations.js
+++ basics/operations.js
@@ -9,3 +9,11 @@ console.log(2-2);
 console.log(2+-2);
 console.log(2/2);
 console.log(2%2);
+
+let nigga1 = "vasu";
+
+
+let nigga2 = " vansh";
+let nigga3 = nigga1 + nigga2;
+
+console.log(nigga3);
