a73b05304198dfad087baf0b975ca6ae32b154df
vinayydv3695 <vinayydv343@gmail.com>
2024-06-23 18:56:47 +0530

    Javascript operations

--- basics/operations.js
+++ basics/operations.js
@@ -17,3 +17,20 @@ let nigga2 = " vansh";
 let nigga3 = nigga1 + nigga2;
 
 console.log(nigga3);
+
+
+console.log(1+"34")
+console.log("23"+3);
+console.log("23"+3+4);
+console.log("23"+3);
+console.log((2+3)+"45");
+
+console.log(+true)
+ // it will give 1 
+ // error
+
+
+let zura1 = zura2 = zura3 = 45;
+let age = 100
+age++
+console.log(age)
