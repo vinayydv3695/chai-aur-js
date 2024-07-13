16455c5d2007bc7ef7c6f41512cac8ff6fd70f83
vinayydv3695 <vinayydv343@gmail.com>
2024-07-13 13:06:04 +0530

    basic concpt of stack and heap

---
+++ basics/stackandheap.js
@@ -0,0 +1,19 @@
+// stack primitive  , Heap non primitive
+let myyoutubename = "katsura"
+let anothername  = myyoutubename
+anothername = "zurako"
+console.log(anothername)
+console.log(myyoutubename)
+
+
+let user1 = {
+    onlyfans: "sujal@gmail.com".
+    password = "konohentai"
+}
+
+
+
+let user2  = user1
+user2.onlyfans  = "vasu@gmail.com"
+console.log(user1.onlyfans)
+console.log(user2.onlyfans)
