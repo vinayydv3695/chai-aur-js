86301917002ccdd918faeb58816da69cc79924d5
vinayydv3695 <vinayydv343@gmail.com>
2024-07-13 13:05:27 +0530

    summary of all datatypes

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+const score = 300
+const valuescore = false
+const isloggedin = false
+const  temp = null
+let anything = undefined
+
+
+const id = Symbol('123')
+const anotherid = Symbol('456')
+
+console.log(id === anotherid)
+const bignumver = 234565432345676543245n
+
+
+
+const heroes = ["luffy","gintoki","zurako"]
+let myObj = {
+    name:  "himmel"
+
+}
+
+const myFuntion = function(){
+    console.log("hello bro")
+}
+    console.log(typeof(bignumver))
+
+    console.log(typeof temp)
+    console.log(typeof myFuntion)
+
