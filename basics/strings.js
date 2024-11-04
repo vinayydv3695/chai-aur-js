72f1630e6e4eab09a0fe41ac36493b46645eec0d
vinayydv3695 <vinayydv343@gmail.com>
2024-11-04 20:49:51 +0530

    chore: strings basics

---
+++ basics/strings.js
@@ -0,0 +1,15 @@
+const name = "vinay";
+
+const repo = 50;
+
+//console.log(name + repo "Value")
+
+console.log(" hello my   is ${repo}  and mine is ${name} ");
+
+console.log("hello ${name}");
+
+const namen = new String("vinay");
+
+console.log(namen[0]);
+
+console.log(name.__proto__);
