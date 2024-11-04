423fb77c712476163df725fdc2fd94be963ee32a
vinayydv3695 <vinayydv343@gmail.com>
2024-11-04 19:24:51 +0530

    .

--- basics/varaibles.js
+++ basics/varaibles.js
@@ -1,13 +1,12 @@
-const bka = "ni"
+const bka = "ni";
 let accountEmail = "kamisama@gmail.com";
 var accountpassword = "L8QLYCQCP";
- accountcCity = "EDO"; 
- let accountState;
+accountcCity = "EDO";
+let accountState;
 
 // accountID = 23 not allowed (comment explaining why this is not allowed)
 // never use var for declaring varaibles always use let be ause var used in acnient times .
-console.log(accountID); 
-console.table([accountID, accountpassword, accountcCity,accountState]); 
-console.log(accountEmailgr
-
+console.log(accountID);
+console.table([accountID, accountpassword, accountcCity, accountState]);
+console.log(accountEmail);
 // if you don,t assign any values to the any varaibles then ti will show undefined
