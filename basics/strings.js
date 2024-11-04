7fa98f1270d4104abaf37a94c8a6449c2a054a48
vinayydv3695 <vinayydv343@gmail.com>
2024-11-04 21:06:00 +0530

    chore: strings basics

--- basics/strings.js
+++ basics/strings.js
@@ -12,4 +12,26 @@ const namen = new String("vinay");
 
 console.log(namen[0]);
 
-console.log(name.__proto__);
+console.log(namen.__proto__);
+console.log(namen.length);
+console.log(namen.toUpperCase());
+console.log(namen.charAt(2));
+console.log(namen.indexOf("y"));
+
+const newString = namen.substring(0, 4);
+
+const dusriString = namen.substring((2, 4));
+
+console.log(dusriString);
+
+const stringnew = "  vinay  ";
+console.log(stringnew);
+console.log(stringnew.trim());
+
+const url = "https://google.com/cornhub%20.com";
+url.replace("%20", "-");
+
+console.log(url);
+
+url.includes(cornhub);
+url.split(google);
