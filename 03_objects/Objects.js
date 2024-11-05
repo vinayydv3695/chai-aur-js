154a82ae3c9b8393d3fc9c8e3f2e1ffa26ff3574
vinayydv3695 <vinayydv343@gmail.com>
2024-11-05 21:43:54 +0530

    chore: Basics of objects  compleeted

--- 03_objects/Objects.js
+++ 03_objects/Objects.js
@@ -1,12 +1,37 @@
-// singeton
-// objects literals
-// object.create
+// singleton
+// Object.create
 
-const jsuser = {
-  name: "Vinay",
+// object literals
+
+const mySym = Symbol("key1");
+
+const JsUser = {
+  name: "Hitesh",
+  "full name": "Hitesh Choudhary",
+  [mySym]: "mykey1",
   age: 18,
-  lastlogin: Moday,
+  location: "Jaipur",
+  email: "hitesh@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
 };
 
-console.log(jsuser."lastlogin");
-console.log(jsuser."lastlogin");
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com";
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com";
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JS user");
+};
+JsUser.greetingTwo = function () {
+  console.log(`Hello JS user, ${this.name}`);
+};
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
