005de0a94f418597b09a52000e55ce2bce56d982
vinayydv3695 <vinayydv343@gmail.com>
2024-11-05 21:41:55 +0530

    chore: Basics of objects

---
+++ 03_objects/Objects.js
@@ -0,0 +1,12 @@
+// singeton
+// objects literals
+// object.create
+
+const jsuser = {
+  name: "Vinay",
+  age: 18,
+  lastlogin: Moday,
+};
+
+console.log(jsuser."lastlogin");
+console.log(jsuser."lastlogin");
