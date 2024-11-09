d956abffdf02358e2c3099ab4ff14dccd6d09631
vinayydv3695 <vinayydv343@gmail.com>
2024-11-09 21:43:48 +0530

    chore: objects apis and de structure

---
+++ 03_objects/lastObjects.js
@@ -0,0 +1,29 @@
+// Object De structure
+
+const course = {
+    name: "js",
+  price: "234",
+  instructor: " hiteshsir",
+};
+
+const { instructor } = course;
+
+console.log(instructor);
+console.log(instructor);
+
+// will be  in react 
+const navbar = {} => {
+
+  navbar(company = "hitesh")
+}
+
+
+
+
+
+
+
+
+
+
+
