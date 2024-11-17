2dd426af4432fd710a8738c940f611f1050170a3
vinayydv3695 <vinayydv343@gmail.com>
2024-11-17 10:39:44 +0530

    chore: starting funtions and parameters

---
+++ 03_basics/funtions.js
@@ -0,0 +1,52 @@
+console.log("V");
+console.log("Y");
+console.log("B");
+console.log("I");
+console.log("N");
+// The uses of funtions
+function sayMyname() {
+  console.log("V");
+  console.log("Y");
+  console.log("B");
+  console.log("I");
+  console.log("N");
+}
+
+sayMyname();
+
+// How to peint two numbers values using funtions
+
+
+function add2num(num1, num2){
+	console.log(num1 + num2);
+	let result = num1 + num2
+	console.log("vinay")
+
+return result;
+
+
+}
+
+add2num((3,4)
+
+const   result = add2num(3,5)
+
+console.log("result :"  , result);
+ 
+
+
+
+function loguser( username){
+	return '${username}  just log in'
+
+	console.log(loguser("vinay"))
+ 
+// If we don't even pass a value then it will give undefined  we can counter it by using if else statement. 
+	console.log(loguser())
+
+
+
+
+
+
+}
