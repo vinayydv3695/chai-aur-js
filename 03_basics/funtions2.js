3c5b3d52d691943c7ce80b64b921304b704e1bf2
vinayydv3695 <vinayydv343@gmail.com>
2024-11-17 19:52:41 +0530

    chore: starting with funtions and objects

---
+++ 03_basics/funtions2.js
@@ -0,0 +1,28 @@
+function calcprice (...num 1){
+
+	return num1
+	
+	
+}
+// Rest 
+console.log(calcprice(200,34,34))
+
+
+
+const user  = {
+	username: "vinay",
+	price: 199
+}
+function handelobj(anyobject) {
+	console.log('Username is ${anyobject.username} and the price is $ {anyobject.price }');
+
+}
+const  mynewarr = =[ 200,40,045]
+
+function retuensecvla(getarray)
+	{
+	return getarray[1]
+
+}
+
+
